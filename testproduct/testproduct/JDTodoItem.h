d8e9d123df8b61b1a66ca23300975c4b60831159
Joshua Delrio <joshua.delrio@jadedpixel.com>
2014-02-11 09:50:14 -0500

    Added properties

---
+++ testproduct/testproduct/JDTodoItem.h
@@ -0,0 +1,17 @@
+//
+//  JDTodoItem.h
+//  testproduct
+//
+//  Created by Joshua Delrio on 2/11/2014.
+//  Copyright (c) 2014 Joshua Delrio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDTodoItem : NSObject
+
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, assign) BOOL completed;
+
+@end
---
+++ testproduct/testproduct/JDTodoItem.m
@@ -0,0 +1,13 @@
+//
+//  JDTodoItem.m
+//  testproduct
+//
+//  Created by Joshua Delrio on 2/11/2014.
+//  Copyright (c) 2014 Joshua Delrio. All rights reserved.
+//
+
+#import "JDTodoItem.h"
+
+@implementation JDTodoItem
+
+@end
